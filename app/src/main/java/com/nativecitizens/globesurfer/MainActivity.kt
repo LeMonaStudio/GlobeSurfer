6f7d6594bc0e92be14ade226d1f65c5c7ef47efc
Musa Lawrence <musa.lawrence93@gmail.com>
2022-11-09 03:58:48 +0100

    Initial commit

---
+++ app/src/main/java/com/nativecitizens/globesurfer/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.nativecitizens.globesurfer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nativecitizens.globesurfer.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+
+@AndroidEntryPoint
+class MainActivity @Inject constructor(): AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
